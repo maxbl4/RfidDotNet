6e618768178e7aabc6669a96a75fa9c4e9993db1
Max <maxbl4@gmail.com>
2019-11-27 21:09:02 +0300

    Update logging and fix dockerfile

--- maxbl4.RfidDotNet.AlienTech.Simulator/Program.cs
+++ maxbl4.RfidDotNet.AlienTech.Simulator/Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Threading.Tasks;
 using maxbl4.RfidDotNet.AlienTech.ReaderSimulator;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace maxbl4.RfidDotNet.AlienTech.Simulator
 {
@@ -12,7 +13,8 @@ namespace maxbl4.RfidDotNet.AlienTech.Simulator
         static async Task Main(string[] args)
         {
             var options = ReadOptions(args);
-            Console.WriteLine($"Starting Alien reader protocol simulator on {options.ListenOn}");
+            var logger = Log.ForContext<Program>();
+            logger.Information($"Starting Alien reader protocol simulator on {options.ListenOn}");
             var readerEndpoint = IPEndPoint.Parse(options.ListenOn);
             var simulator = new SimulatorListener(readerEndpoint);
             var tagListHandler = new TagListHandler(options);
@@ -22,11 +24,11 @@ namespace maxbl4.RfidDotNet.AlienTech.Simulator
                 .Select(x => new Tag{TagId = x, DiscoveryTime = DateTime.UtcNow, LastSeenTime = DateTime.UtcNow, ReadCount = 1})
                 .ToArray();
             if (options.RandomTags)
-                Console.WriteLine($"Will return random tags");
+                logger.Information($"Will return random tags");
             else
-                Console.WriteLine($"Found {tags.Length} tags in options");
+                logger.Information($"Found {tags.Length} tags in options");
             tagListHandler.ReturnContinuos(tags);
-            Console.WriteLine($"Waiting for connections on {simulator.ListenEndpoint}, press CTRL+C to stop");
+            logger.Information($"Waiting for connections on {simulator.ListenEndpoint}, press CTRL+C to stop");
             Console.CancelKeyPress += (sender, args) => simulator.Dispose();
             await simulator.ListenTask;
         }
@@ -38,7 +40,10 @@ namespace maxbl4.RfidDotNet.AlienTech.Simulator
                 .AddEnvironmentVariables()
                 .AddCommandLine(args ?? new string[0])
                 .Build();
-            return config.Get<SimulatorOptions>();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+            return config.GetSection(nameof(SimulatorOptions)).Get<SimulatorOptions>();
         }
     }
 }
